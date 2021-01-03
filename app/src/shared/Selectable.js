d07bdea1f860189c81f7c71cf821fe851c6753cc
MarkoVlaic <vlaic.marko@gmail.com>
2021-01-03 14:14:17 +0100

    Changed the selectable color scheme

--- app/src/shared/Selectable.js
+++ app/src/shared/Selectable.js
@@ -14,29 +14,33 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     maxWidth: 150,
     //backgroundColor: '#333',
-    borderColor: '#246b44',
-    backgroundColor: '#2a7d4f',
+    /*borderColor: '#246b44',
+    backgroundColor: '#2a7d4f',*/
+    backgroundColor: '#e7e7e7',
+    borderColor: '#d7d7d7',
     borderRadius: 5,
     borderWidth: 1,
   },
   active: {
-    backgroundColor: '#e7e7e7',
-    borderColor: '#d7d7d7',
+    /*backgroundColor: '#e7e7e7',
+    borderColor: '#d7d7d7',*/
+    borderColor: '#246b44',
+    backgroundColor: '#2a7d4f',
     borderWidth: 1,
   },
   textDefault: {
     fontSize: 18,
-    borderBottomColor: '#e7e7e7',
     borderBottomWidth: 4,
     borderRadius: 5,
-    color: '#fff',
+    borderBottomColor: '#3CB371',
+    color: '#333',
     textAlign: 'center',
     textAlignVertical: 'center',
     padding: 4,
   },
   textActive: {
-    borderBottomColor: '#3CB371',
-    color: '#333',
+    borderBottomColor: '#e7e7e7',
+    color: '#fff',
   },
 });
 
